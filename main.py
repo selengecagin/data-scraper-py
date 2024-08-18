487930bb99a72928812e3c27923cdea8d43ba731
selengecagin <selengecagin@gmail.com>
2024-08-18 21:58:25 +0300

    Append title and link to all_articles list

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install())
 try:
     # Open the JSONL file in append mode
     with open("titles.jsonl", "a") as f:
-        all_titles  = []
+        all_articles  = []
         for page in range(1, 21):  # Loop through the first 20 pages
             # Navigate to the page
             url = f"https://code-maze.com/latest-posts-on-code-maze/page/{page}/"
@@ -25,9 +25,6 @@ try:
             # Find <h2> elements with class "entry-title"
             titles = driver.find_elements(By.CSS_SELECTOR, "h2.entry-title")
 
-            # Add the titles to all_titles list
-            all_titles.extend(titles)
-
             # Get the href value of the first <a> element in the <h2> element
             for title in titles:
                 try:
@@ -39,6 +36,8 @@ try:
                     title_link = title.find_element(By.CSS_SELECTOR, "a").get_attribute("href")
                     print(title_link)
 
+                    all_articles.append({"title": title_text, "link": title_link})
+
                     # Save the data to a JSONL file
                     json_line = json.dumps({"title": title_text, "link": title_link})
                     f.write(json_line + "\n")
@@ -46,7 +45,7 @@ try:
                     print(f"Error processing title: {e}")
 
         # Print the total number of titles
-        print(f"Total number of titles: {len(all_titles)}")
+        print(f"Total number of titles: {len(all_articles)}")
 
 
 finally:
